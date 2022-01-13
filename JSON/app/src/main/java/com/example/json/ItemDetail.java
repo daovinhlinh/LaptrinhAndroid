348756ecc8bd7818ad2c966e586bf0ff1133b7af
Dao Vinh Linh <linh142000@gmail.com>
2022-01-14 00:03:25 +0700

    Add view

--- JSON/app/src/main/java/com/example/json/ItemDetail.java
+++ JSON/app/src/main/java/com/example/json/ItemDetail.java
@@ -6,25 +6,31 @@ import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
 public class ItemDetail extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_item_detail);
         ImageView avatar = findViewById(R.id.avatar);
         TextView username =  findViewById(R.id.username);
+        TextView name =  findViewById(R.id.name);
         TextView email =  findViewById(R.id.email);
         TextView address =  findViewById(R.id.address);
         TextView phone =  findViewById(R.id.phone);
         TextView company =  findViewById(R.id.company);
 
-        String getAvatar = getIntent().getStringExtra("avatar");
-        String getUsername = getIntent().getStringExtra("username");
-        String getName = getIntent().getStringExtra("name");
-        String getEmail = getIntent().getStringExtra("email");
-        String getAddress = getIntent().getStringExtra("avatar");
-        String getPhone = getIntent().getStringExtra("avatar");
+        Picasso.get().load("https://lebavui.github.io" + getIntent().getStringExtra("avatar")).into(avatar);
+        username.setText(getIntent().getStringExtra("username"));
+        name.setText(getIntent().getStringExtra("name"));
+        email.setText(getIntent().getStringExtra("email"));
+        address.setText(getIntent().getStringExtra("address"));
+        phone.setText(getIntent().getStringExtra("phone"));
+        company.setText(getIntent().getStringExtra("company"));
+
 
     }
 }
